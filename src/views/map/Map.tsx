2627a932f92ca8cc913c8ee377efb5ffd86eadc7
ngdathd <ngdathd@gmail.com>
2021-06-20 00:41:45 +0700

    react native maps

--- src/views/map/Map.tsx
+++ src/views/map/Map.tsx
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react';
 import {Button, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 import MyNavigator from 'utils/MyNavigator';
 
 import {mapStyles} from './styles';
@@ -12,6 +13,16 @@ class Map extends PureComponent {
                     title="Search"
                     onPress={() => {
                         MyNavigator.navigate('Search');
+                    }}
+                />
+                <MapView
+                    provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                    style={mapStyles.container}
+                    region={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
                     }}
                 />
             </View>
